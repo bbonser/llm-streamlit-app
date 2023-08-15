f7f8eeccbb92ddf2dc3b9f4be64baf36d8a5920d
Brett Bonser <bbonser89@gmail.com>
2023-08-15 17:36:47 -0400

    update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 import streamlit as st
 import os
+import requests
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -11,11 +12,25 @@ load_dotenv()
 # Access environment variables
 api_key = os.environ['OPENAI_API_KEY']
 
-st.title('Ask me anything!')
-prompt = st.text_input("Enter your text here and hit Enter:")
-
 llm = OpenAI(temperature=0.6)
 
+st.title('Ask me anything!')
+
+# Initialize chat history
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
+prompt = st.text_input("Enter your text here and hit Enter:")
+
 if prompt:
-    response = llm(prompt)
+    with st.spinner('Generating response...'):
+        response = llm(prompt)
+        # Add the user's question and the bot's response to the chat history
+        st.session_state['chat_history'].append({'user': prompt, 'bot': response})
     st.write(response)
+
+# Display chat history in the sidebar
+st.sidebar.title("Chat History")
+for chat in st.session_state['chat_history']:
+    st.sidebar.markdown(f"**You:** {chat['user']}")
+    st.sidebar.markdown(f"**Bot:** {chat['bot']}")
